cc0a635f049086deb0e848508372801dbc34dfda
Fabio Zanini <iosonofabio@gmail.com>
2010-05-05 11:47:34 +0200

    Corrected bugs in the Data class: also, now it works correctly with the Malvern instrument for both SLS and DLS as long as one respects the input syntax

--- @Data/set_general_parameters_Malvern.m
+++ @Data/set_general_parameters_Malvern.m
@@ -3,7 +3,7 @@
 % Malvern is so user-friendly that the instrument does not say anything
 % Thus, we just generate the information according to our knowledge
 %=========================================================================================
-function [ lambda unit_lambda n_set ] = read_general_file_Malvern ( path, runstart )
+function [ lambda unit_lambda n_set ] = set_general_parameters_Malvern
 
  lambda		= 632;				% is it a HeNe laser?
  unit_lambda	= 'nm';
