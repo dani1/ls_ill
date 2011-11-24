cc20a4fdf54ebf31428faae2b9a00aead11e332b
Daniel Soraruf <daniel.soraruf@gmail.com>
2011-11-24 14:45:58 +0100

    Modified 2nd version for basic working with DLS / SLS.
    Furthermore compiled Contin for macOSX 32 bit

--- Contin/compile_contin.m
+++ Contin/compile_contin.m
@@ -1,1 +1,3 @@
-mex -lool -lgsl -lgslcblas -lm contin.c
+%change -I_folder to include folders in which have been installed ool and
+%gsl
+mex -I/usr/local/include -lool -lgsl -lgslcblas -lm contin.c
