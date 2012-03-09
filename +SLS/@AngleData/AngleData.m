2ea4479370250ebd1b87290acc64d794afa3dd0d
Daniel Soraruf <daniel.soraruf@gmail.com>
2012-03-09 15:59:15 +0100

    better comments

--- +SLS/@AngleData/AngleData.m
+++ +SLS/@AngleData/AngleData.m
@@ -8,9 +8,7 @@
 classdef AngleData < handle
 	properties
 		scatt_angle;
-		%----------------------------------------------------------------------
 		% allocate count to 0x0 array of struct
-		%----------------------------------------------------------------------
 		count = struct([]);
 		mean_count_rate;
 		mean_monitor_intensity;
@@ -21,16 +19,16 @@ classdef AngleData < handle
 		dKcR;
 	end
 	methods
-		%======================================================================
+		%----------------------------------------------------------------------
 		% constructor : set angle
-		%======================================================================
+		%----------------------------------------------------------------------
 		function self = AngleData(scatt_angle)
 			self.scatt_angle = scatt_angle;
 		end
-		%======================================================================
+		%----------------------------------------------------------------------
 		% add struct of one count to class struct (members scatt_angle,
 		% count_rate, minitor_intensity, error_count_rate);
-		%======================================================================
+		%----------------------------------------------------------------------
 		function add(self, count_struct)
 			len = length(self.count);
 			if count_struct.scatt_angle == self.scatt_angle;
@@ -41,9 +39,8 @@ classdef AngleData < handle
 				self.count(len + 1).temperature = count_struct.temperature;
 			end
 		end
-		%======================================================================
+
 		% calculate mean count rate and monitor intensity with errors (std_dev)
-		%======================================================================
 		function e = calc_error(self)
 			e = 0;
 			for i = 1 : length(self.count)
@@ -56,16 +53,12 @@ classdef AngleData < handle
 			%self.check_cr();
 			if ~isempty(self.count)
 				len = length(self.count);
-				%--------------------------------------------------------------
 				% set values to zero
-				%--------------------------------------------------------------
 				mean_cr = 0;
 				mean_i_mon = 0;
 				error_mean_cr = 0;
 				error_mean_i_mon = 0;
-				%--------------------------------------------------------------
 				% calculate mean values
-				%--------------------------------------------------------------
 				for i = 1 : len
 					mean_cr = mean_cr + self.count(i).count_rate;
 					mean_i_mon = mean_i_mon + self.count(i).monitor_intensity;
@@ -94,25 +87,25 @@ classdef AngleData < handle
 				self.mean_monitor_intensity = 0;
 			end
 		end
-		%======================================================================
+		%----------------------------------------------------------------------
 		% Error propagation function for dR = 1/R !!!
-		%======================================================================
+		%----------------------------------------------------------------------
 		function dR = R_error_propagation(self, standard, solvent, R_solution, dR_solution, angle_index)
 			R_tol = standard.ratio(angle_index);
 			dR_tol = standard.error_ratio(angle_index)* R_tol / 100;
 			R_solv = solvent.ratio(angle_index);
 			dR_solv = solvent.error_ratio(angle_index)* R_solv / 100;
 			RR = standard.rayleigh_ratio(angle_index);
-			%------------------------------------------------------------------
+
 			% calculate error of 1 over R using gaussian error propagation
-			%------------------------------------------------------------------
 			dR =sqrt((dR_tol / (R_solution - R_solv))^2 + (R_tol * dR_solv /...
 			   	(R_solution -R_solv)^2)^2 + (R_tol * dR_solution /...
 			   	(R_solution - R_solv)^2)^2)/RR;
 		end
 		%======================================================================
-		% calculate Kc/R of certain angle
+		% calculate Kc/R at certain angle
 		% protein_conc in mg/l !!!
+		% dn_over_dc normally in [0.1:0.3]
 		% TODO: error propagation
 		%======================================================================
 		function calc_kc_over_r(self, standard, solvent, protein_conc, dn_over_dc, instrument)
@@ -124,44 +117,38 @@ classdef AngleData < handle
 			%wavelength = 0.00006328 ; % cm
 			number_avogadro = Constants.Na; 
 			
-			%------------------------------------------------------------------
 			% calculate optical constant
-			%------------------------------------------------------------------
 			K = (2 * pi * dn_over_dc * standard.refraction_index )^2 /...
 				(wavelength^4 * number_avogadro);
 			%sample_excess_count_rate = (count_rate - solvent.count_rate(angle_index)) ...
 			%/ standard.count_rate(angle_index);
-			%------------------------------------------------------------------
+
 			% calculate ratio of solution
-			%------------------------------------------------------------------
 			R_solution =  self.mean_count_rate * sin( self.scatt_angle * pi/180)...
 			   	/ self.mean_monitor_intensity;
 			dR_solution = sqrt((self.error_mean_count_rate / self.mean_monitor_intensity...
 				)^2 + (self.mean_count_rate * self.error_mean_monitor_intensity /...
 				self.mean_monitor_intensity^2)^2)*sin(self.scatt_angle * pi/180);
 
-			%------------------------------------------------------------------
 			% R needed for Kc/R, calculated using ratios saved in solvent
 			% and standard file
-			%------------------------------------------------------------------
 			R = (R_solution - solvent.ratio(angle_index) ) / standard.ratio(angle_index)...
 			   	* standard.rayleigh_ratio(angle_index);
 			dR = self.R_error_propagation(standard, solvent, R_solution, dR_solution, angle_index);
 			one_over_R = 1 / R;
 			rel_error = dR / one_over_R;
-			%------------------------------------------------------------------
+
 			% calculate KcR and relative error
 			% TODO: calculate errors and check standard deviations
-			%------------------------------------------------------------------
 			self.KcR = K * protein_conc/ R;
 			self.dKcR = K * protein_conc * dR;
 			%disp(self.dKcR / self.KcR)
 			
 		end
-		%======================================================================
+		%----------------------------------------------------------------------
 		% show angle, mean count rate and single count rates for checks
-		% TODO: rewrite for new save method
-		%======================================================================
+		% TODO: rewrite for new save method. not necessary
+		%----------------------------------------------------------------------
 		function show_cr(self)
 			a = self.scatt_angle;
 			cr = [self.count.count_rate];
@@ -172,10 +159,10 @@ classdef AngleData < handle
 			end
 			disp(str);
 		end
-		%======================================================================
+		%----------------------------------------------------------------------
 		% check that count_rate does not have strange scattering (e.g.dust)
-		% TODO: rewrite for new save technique.
-		%======================================================================
+		% TODO: rewrite for new save technique. !!! not explicitly needed
+		%----------------------------------------------------------------------
 		function check_cr(self)
 			cr_tolerance_limit = 0.05;
 			cr = self.count_rates;
