7e000f0cc6704148daa1438b4bf742eab5713a0f
Daniel <daniel.soraruf@student.uni-tuebingen.de>
2012-04-27 18:45:09 +0200

    working experimental branch

--- +SLS/@Point/Point.m
+++ +SLS/@Point/Point.m
@@ -1,6 +1,5 @@
 classdef Point
 % This class describes a single SLS datapoint
-
  properties
 
   Protein					% what protein?
@@ -76,7 +75,6 @@ classdef Point
   function dKcR = get.dKcR ( self )
    dKcR		= self.KcR / self.KcR_raw * self.dKcR_raw;
   end
-
   function X_T = get.X_T ( self )
   % the osmotic isothermal compressibility is related to the structure factor at
   % vanishing Q by the following formula:
@@ -94,6 +92,13 @@ classdef Point
 
   function dX_T = get.dX_T ( self )
    dX_T = self.X_T .* self.dKcR ./ self.KcR;			% TODO: errors only come from KcR
+  end
+  %============================================================================
+  % correct intensity values based on comparison
+  %============================================================================
+  function correct_attenuator_intensity ( self )
+	  attenuators = self.Instrument.attenuators;
+	  RawData = SlsData;
   end
 
  end
