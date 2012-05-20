9a82fb34aa7c992ce938bf73d1378ab9dc55f0cf
Daniel Soraruf <daniel.soraruf@gmail.com>
2012-05-20 12:49:59 +0200

    added container (folder) LIT with subclass BSA which gives information about the protein itself (from literature) [Taken from older version (Master branch)]

---
+++ +LIT/@BSA/BSA.m
@@ -0,0 +1,33 @@
+%============================================================================
+%
+% BSA class
+% 
+% This class contains information about the BSA molecule. I can present
+% results coherently using these values.
+%
+%============================================================================
+classdef BSA
+ properties ( Access = public, Constant )					% with the 'Constant' attribute, they can be called from outside without an instance
+
+ M			=	66411
+ Unit_M			=	'Da'
+ dndc_H2O		=	[ 183 181 ]
+ dndc_100mM_NaCl	=	180
+ Unit_dndc		=	'mul/g'
+ dndc_lambda		=	'632 nm'
+ D0			=	6.0
+ Unit_D0		=	'A^2/ns'
+ v0			=	1.140e-3
+ Unit_v0		=	'l/g'
+ R			=	3.60
+ Unit_R			=	'nm'
+
+ % buffers properties
+ CH3COOH_M_g_mol	=	60.05;
+ NaCH3COO_M_g_mol	=	82.03;
+
+ %end of properties
+ end
+
+% end of LIT class
+end
